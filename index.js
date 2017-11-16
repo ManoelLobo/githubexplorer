f74cf994758fde257a1a626af7adec3a401631c5
Manoel Lobo <manoel.lobo@gmail.com>
2017-11-16 11:40:31 -0300

    GoNative - Desafio 02 - repo init

---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('desafio02', () => App);
---
+++ src/App.js
@@ -0,0 +1,13 @@
+/**
+ * GoNative - Desafio 02
+ * Manoel Thiago Nogueira
+ */
+
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => (
+  <View />
+);
+
+export default App;
